cf0f8dab4934aca1d14e95cae4f65778c666189b
Wayne Kenneth Seymour III <wayneseymour@gmail.com>
2017-02-28 11:34:44 -0500

    Updated my impl, now it has no logic branches just like the official impl

--- fp-js-workshop-call.js
+++ fp-js-workshop-call.js
@@ -3,23 +3,14 @@
 // defined directly on them.
 // Do not match values inherited from prototypes.
 
-const duckCount = (...xs) => {
-  return xs.reduce(
-    (accum, curr) => {
-      if (curr.hasOwnProperty) if (curr.hasOwnProperty('quack')) accum = accum + 1;
-        else if (Object.prototype.hasOwnProperty.call(curr, 'quack')) accum = accum + 1;
+const duckCount = (...xs) => xs.filter(obj => Object.prototype.hasOwnProperty.call(obj, 'quack')).length;
 
-      return accum;
-    },
-    0
-  );
-};
+const n = Object.create(null);
+n.quack = true;
 
-// const n = Object.create(null);
-// n.quack = true;
-//
-// const result = duckCount({ quack: true }, { quack: true }, n);
-// console.log('result: ', result);
+const result = duckCount({ quack: true }, { quack: true }, n);
+console.log('result: ', result);
+
 module.exports = duckCount;
 
 // Official Impl:
